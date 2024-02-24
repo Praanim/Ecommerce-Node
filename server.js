e77c97095dd3b88fb848ed6c231bf2dcaf643aac
Pranim Singh Thakuri <pranimsinghthakuri@192.168.1.4>
2024-02-24 08:44:08 +0545

    ENV FILE ADDED

--- server.js
+++ server.js
@@ -1,41 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const PORT= 3000;
-const UserRoutes = require("./src/routes/user_routes")
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const UserRoutes = require("./src/routes/user_routes");
 const CategoryRoutes = require("./src/routes/category_routes");
 const ProductRoutes = require("./src/routes/product_routes");
 const CartRoutes = require("./src/routes/cart_routes");
-
-
-
+require("dotenv").config();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //app routes
-app.use("/api/user",UserRoutes);
-app.use("/api/category",CategoryRoutes);
-app.use("/api/product",ProductRoutes);
-app.use("/api/cart",CartRoutes);
-
-
-
+app.use("/api/user", UserRoutes);
+app.use("/api/category", CategoryRoutes);
+app.use("/api/product", ProductRoutes);
+app.use("/api/cart", CartRoutes);
 
 //db-->name = ecommerce
-const mongoDbPath= "mongodb+srv://pranimsingh7:pranim2580@cluster0.scwvxin.mongodb.net/ecommerce?retryWrites=true&w=majority";
-mongoose.connect(mongoDbPath).then(
-    function(){
-        app.get('/',function(req,res){
-            res.send("This is home page");
-        });
-        
-    }
-);
+const mongoDbPath = process.env.MONGO_DB_PATH;
+mongoose.connect(mongoDbPath).then(function () {
+  app.get("/", function (req, res) {
+    res.send("This is home page");
+  });
+});
 
+const PORT = process.env.PORT || 3001;
 
-
-app.listen(PORT,function(){
-    console.log(`Server Started at PORT: ${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Server Started at PORT: ${PORT}`);
+});
